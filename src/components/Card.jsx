4eb8277318700466d1b0c28097764e3f1b54533e
Ярослав <fedyaroslavv@gmail.com>
2026-03-18 01:08:29 +0300

    add styling and searchBar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 
 function Card({product}){
   const [showDetails, setShowDetails] = useState(false)
+  const [quanity, setQuantity] = useState(0)
 
   return (
-      <>
+      <div className='card'>
         <p>Название: {product.name}</p>
         <p>Цена: {product.price}$</p>
         {showDetails && <p>{product.description}</p>}
         <button onClick={() => setShowDetails(prev => !prev)}>{showDetails ? 'Скрыть' : 'Подробнее'}</button>
-      </>
+        <button onClick = {() => setQuantity(prev => prev + 1)}>Add to Cart</button>
+        <p>Количество: {quanity}</p>
+        <button onClick = {() => setQuantity(0)}>Удалить продукт</button>
+      </div>
   )
 }
 
--- src/components/MiniShop.jsx
+++ src/components/MiniShop.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 import Card from './Card'
 
 function MiniShop({products}){
+  const [input, setInput] = useState('')
+
+  const filteredProducts = products.filter(product => product.name.toLowerCase().includes(input.toLowerCase()))
 
   return (
       <>
-        {products.map((product) =>
+        <input
+            className='searchBar'
+            placeholder='Поиск продукта'
+            type="text"
+            value = {input}
+            onChange = {(e) => setInput(e.target.value)}
+        />
+        {filteredProducts.map((product) =>
             <Card
                 key={product.id}
                 product = {product}
